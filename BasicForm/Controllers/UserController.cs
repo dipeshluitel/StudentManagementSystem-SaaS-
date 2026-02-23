a259a48a6aea9ee3087e94a69f1ccfaf18ce997b
Dipesh <dipeshluitel40@gmail.com>
2026-02-23 09:51:31 +0545

    update(navbar/footer): navbar and footer updated

---
+++ BasicForm/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BasicForm.Models;
+namespace BasicForm.Controllers;
+
+public class UserController:Controller
+{
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create(MyForm myForm)
+    {
+        if (ModelState.IsValid)
+        {
+            return View("Success", myForm);
+        }
+        return View(myForm);
+    }
+}
---
+++ BasicForm/Models/MyForm.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BasicForm.Models;
+
+public class MyForm
+{
+    // [Required]
+    public String Fname { get; set; }
+    
+    [Required]
+    public String Lname { get; set; }
+    
+    [Required]
+    [EmailAddress]
+    public String Email { get; set; }
+    
+    [Required]
+    [Range(0, 100)]
+    public int Age { get; set; }
+}
